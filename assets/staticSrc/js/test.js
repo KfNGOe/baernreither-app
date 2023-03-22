a553c014138aa56c21203d2fd016eca14934f770
rh <richard.hoermann@inode.at>
2023-03-22 17:31:20 +0100

    test xml-js in browser

--- assets/staticSrc/js/test.js
+++ assets/staticSrc/js/test.js
@@ -1,2 +1,39 @@
-var convert = require("xml-js") ;
-console.log(convert.xml2json("<xml>test</xml>")) ;
+const { EnapsoGraphDBClient } = require('@innotrade/enapso-graphdb-client') ;
+//var convert = require("xml-js") ;
+
+//console.log(convert.xml2json("<xml>test</xml>")) ;
+
+//console.log(EnapsoGraphDBClient) ;
+
+let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
+    baseURL: 'http://localhost:7200',
+    repository: 'Test',
+    triplestore: 'graphdb', // 'graphdb' or 'fuseki' or 'stardog'
+    prefixes: [
+        {
+            prefix: 'entest',
+            iri: 'http://ont.enapso.com/test#'
+        }
+    ],
+    transform: 'toCSV'
+});
+
+//console.log(graphDBEndpoint) ;
+
+graphDBEndpoint.login('admin','root').then((result) => {
+    console.log(result);
+})
+.catch((err) => {
+    console.log(err);
+});
+
+/*
+graphDBEndpoint.query('select * where {?class rdf:type owl:Class filter(regex(str(?class), "http://ont.enapso.com/test#TestClass", "i"))}', { transform: 'toJSON' }).then((result) => {
+        console.log('Read the classes name:\n' + JSON.stringify(result, null, 2));
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+*/
+
+
