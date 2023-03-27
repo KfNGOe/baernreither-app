ee0bfcec446fd8c691a94767b42a1db509c7d794
rh <richard.hoermann@inode.at>
2023-03-27 22:21:01 +0200

    test graphdb

--- assets/staticSrc/js/test.js
+++ assets/staticSrc/js/test.js
@@ -1,5 +1,6 @@
 const { EnapsoGraphDBClient } = require('@innotrade/enapso-graphdb-client') ;
 const { EnapsoGraphDBAdmin } = require('@innotrade/enapso-graphdb-admin');
+var connectFlag = false ;
 //var convert = require("xml-js") ;
 
 //console.log(convert.xml2json("<xml>test</xml>")) ;
@@ -18,16 +19,35 @@ let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
     apiType: 'RDF4J'
 });
 
-console.log('Waiting for GraphDB to start...');    
-do {    
-} while (!graphDBEndpoint.isReady());
+connect = async function() {
+    try {
+        const result =  await graphDBEndpoint.login('admin','root');
+        console.log('result= ', result.success);
 
+        return result.success ;      
+      
+    } catch (err) {
+        console.log('result error = ', err.success);
+    };
+  }
+
+  connect().then(value => {
+    connectFlag = value ;
+    console.log("Connect =", connectFlag) ;
+}) ;
+   
+
+     
+/*
 graphDBEndpoint.login('admin','root').then((result) => {
-    console.log(result);
+    console.log('result= ', result.success);    
 })
 .catch((err) => {
-    console.log(err);
+    console.log('result= ', err.success);
 });
+*
+
+
 
 /*
 graphDBEndpoint.query('select * where {?class rdf:type owl:Class filter(regex(str(?class), "http://ont.enapso.com/test#TestClass", "i"))}', { transform: 'toJSON' }).then((result) => {
---
+++ graphdb-10.2.0/data/users.js
@@ -0,0 +1,17 @@
+{
+  "users" : {
+    "admin" : {
+      "username" : "admin",
+      "password" : "{bcrypt}$2a$10$xe64SKJdDumOC32iH.QBd.9gjU4UvjQ1VYes4EKu3x8Tp0ATYopuW",
+      "grantedAuthorities" : [ "ROLE_ADMIN" ],
+      "appSettings" : {
+        "DEFAULT_INFERENCE" : true,
+        "DEFAULT_VIS_GRAPH_SCHEMA" : true,
+        "DEFAULT_SAMEAS" : true,
+        "IGNORE_SHARED_QUERIES" : false,
+        "EXECUTE_COUNT" : true
+      },
+      "dateCreated" : 1679936069996
+    }
+  }
+}
