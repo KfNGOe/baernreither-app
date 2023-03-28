2ee9d2b8450d13359ec2e4304b062f148b84483e
rh <richard.hoermann@inode.at>
2023-03-28 18:24:13 +0200

    test xml-js

--- assets/staticSrc/js/xml_js.js
+++ assets/staticSrc/js/xml_js.js
@@ -1,17 +1,11 @@
 const fs = require('fs/promises');
 var convert = require('xml-js');
-var xml = '' ;
-/*
-fs.readFile('data/tei/Tagebuch_Baernreither_8.xml', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-*/ 
+var xml = '' ; 
 
 async function readXml() {
-    try {
+    try {        
         const data = await fs.readFile('data/tei/Tagebuch_Baernreither_8.xml', { encoding: 'utf8' });
-        //console.log(data);
+        
         return data;
 
     } catch (err) {
@@ -19,31 +13,11 @@ async function readXml() {
     }
 }
 
-readXml().then(value => {
-    xml = value ;    
-    
-    //var result1 = convert.xml2json(xml, {compact: false, spaces: 4});
-    var result2 = convert.xml2js(xml, {compact: false, spaces: 2});
-    var result1 = '';
-
-    console.log('result json: ', result1, '\n','result js: ', result2);
-
-}) ;
-
-
-  
+(async () => {        
+    await readXml().then(value => {
+        xml = value ;}) ;
+        var result = convert.xml2js(xml, {compact: false, spaces: 2});
+        console.log('result: ', result);
+  })() ; 
   
 
-/*
-var xml =
-'<?xml version="1.0" encoding="utf-8"?>' +
-'<note importance="high" logged="true">' +
-'    <title>Happy</title>' +
-'    <todo>Work</todo>' +
-'    <todo>Play</todo>' +
-'</note>';
-var result1 = convert.xml2json(xml, {compact: false, spaces: 4});
-var result2 = convert.xml2js(xml, {compact: false, spaces: 4});
-
-console.log('result json: ', result1, '\n','result js: ', result2);
-*/
