2ab156ff82d229e7fef39e3571e0cd166d6f8b27
rh1967 <richard.hoermann@inode.at>
2023-03-31 17:09:24 +0200

    install jquery

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 echo "Install graphdb.js"
 sudo bash ./dl_graphdb.sh
 
-echo "Install xml-js"        
-npm i xml-js --save        
+echo "Install xml-js"
+npm i xml-js --save
+
+echo "Install jquery for Node.js"
+npm install jquery
+npm install jsdom
 
 #echo "install localtunnel"
 #npm install -g localtunnel
