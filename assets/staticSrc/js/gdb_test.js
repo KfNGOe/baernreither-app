3db180e847ce862ada30aaef52eead41fb1bbb34
rh <richard.hoermann@inode.at>
2023-07-15 17:38:45 +0200

    build graphdb js

--- assets/staticSrc/js/gdb_test.js
+++ assets/staticSrc/js/gdb_test.js
@@ -1,10 +1,14 @@
 //RDF repository client
+const {RDFRepositoryClient, RepositoryClientConfig} = require('graphdb').repository ;
+
 const endpoint = 'http://localhost:7200' ;
 const readTimeout = 30000 ;
 const writeTimeout = 30000 ;
 const config = new RepositoryClientConfig(endpoint)
-    .setEndpoint('http://localhost:7200/repositories/kfngoe_test')
+    .setEndpoints(['http://localhost:7200/repositories/kfngoe_test'])
+    //.setEndpoint('http://localhost:7200/repositories/kfngoe_test')
     .setReadTimeout(readTimeout)
     .setWriteTimeout(writeTimeout) ;
     
 const repository = new RDFRepositoryClient(config) ;
+console.log('repository: ', repository.repositoryClientConfig) ;
