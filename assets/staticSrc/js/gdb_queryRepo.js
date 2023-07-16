125e2d69fb4403922cd6d9d5dcc160a06accc026
rh <richard.hoermann@inode.at>
2023-07-16 19:56:13 +0200

    build graphdb js

--- assets/staticSrc/js/gdb_queryRepo.js
+++ assets/staticSrc/js/gdb_queryRepo.js
@@ -1,53 +1,69 @@
 const fs = require('fs') ;
-const { EnapsoGraphDBClient } = require('@innotrade/enapso-graphdb-client') ;
-const { EnapsoGraphDBAdmin } = require('@innotrade/enapso-graphdb-admin') ;
-var connectFlag = false ;
-//var convert = require("xml-js") ;
 
-//console.log(convert.xml2json("<xml>test</xml>")) ;
+const RDFMimeType = require('graphdb/lib/http/rdf-mime-type');
+//const SparqlJsonResultParser = require('graphdb/lib/parser/sparql-json-result-parser');
+//const SparqlXmlResultParser = require('graphdb/lib/parser/sparql-xml-result-parser');
+const {TurtleParser} = require('graphdb').parser ;
 
-let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
-    baseURL: 'http://localhost:7200',
-    repository: 'kfngoe_test',
-    prefixes: [
-        {
-            prefix: 'entest',
-            iri: 'http://ont.enapso.com/test#'
-        }
-    ], 
-    //format: 'text/turtle',   
-    triplestore: 'graphdb',
-    version: 10,
-    apiType: 'RDF4J'
-}) ;
+//RDF repository client
+const {RDFRepositoryClient, RepositoryClientConfig} = require('graphdb').repository ;
+const {GetQueryPayload, QueryType} = require('graphdb').query ;
 
-graphDBEndpoint
-    .query(
-        'SELECT ?s ?p ?o WHERE { ?s ?p ?o }',
-        { transform: 'toJSON' }
-    )
-    .then((result) => {
-        console.log(
-            'Read the classes name:\n' + JSON.stringify(result, null, 2)
-        );
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+const query = 'CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}' ;
+//const query = 'SELECT * WHERE { ?s ?p ?o }' ;
 
+const mimeType = RDFMimeType.TURTLE ;
+//const mimeType = RDFMimeType.RDF_JSON ;
+//const mimeType = RDFMimeType.SPARQL_RESULTS_JSON ;
 
-(async () => {    
+const queryType = QueryType.CONSTRUCT ;
+//const queryType = QueryType.SELECT ;
+
+const endpoint = 'http://localhost:7200' ;
+const readTimeout = 30000 ;
+const writeTimeout = 30000 ;
+const config = new RepositoryClientConfig(endpoint)
+    .setEndpoints(['http://localhost:7200/repositories/kfngoe_test'])
+    //.setEndpoint('http://localhost:7200/repositories/kfngoe_test')
+    .setReadTimeout(readTimeout)
+    .setWriteTimeout(writeTimeout) ;
+    
+const repository = new RDFRepositoryClient(config) ;
+console.log('repository: ', repository.repositoryClientConfig) ;
+
+//RDF query
+//repository.registerParser(new TurtleParser()) ;
+//repository.registerParser(new SparqlJsonResultParser()) ;
+
+(async () => {
+    const payload = new GetQueryPayload()
+    .setResponseType(mimeType)
+    .setQuery(query)
+    .setQueryType(queryType)
+    .setLimit(3) ;
+
+    //get namespaced prefixes
+    const prefixes = await repository.getNamespaces() ;
+    console.log('prefixes: ', prefixes) ;
+
     var sparql = fs.readFileSync('assets/staticSrc/sparql/test.rq', 'utf8');
     console.log('sparql data read: ', sparql.length, ' bytes')
     
-    let result = await graphDBEndpoint.query(sparql, {format: 'text/turtle'}).catch((err) => {
+    let result = await repository.query(payload).catch((err) => {
         console.log(err);
-    });
-
-    let result_json = JSON.stringify(result) ;
-
-    fs.writeFileSync('./data/json/test.json', result_json, 'utf8') ;
-        console.log('json data written: ', result_json.length, ' bytes')
-    
+    }) ;
+    //console.log('result: ', result) ;
+    result.on('data', (data) => {
+        // handle data
+        console.log('data: ', data.toString()) ;
+        //let result_json = data.toString() ;
+        let result_ttl = data.toString() ;
+        
+        //fs.writeFileSync('./data/json/test.json', result_json, 'utf8') ;        
+        fs.writeFileSync('./data/ttl/test.ttl', result_ttl, 'utf8') ;
+        //console.log('json data written: ', result_json.length, ' bytes')
+        console.log('ttl data written: ', result_ttl.length, ' bytes')
+    }) ;
 })() ;
+
 
