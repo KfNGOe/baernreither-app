5320bfafa1878ec4b5988b8471a43308322c1f50
rh <richard.hoermann@inode.at>
2023-07-16 17:43:53 +0200

    build graphdb js

--- assets/staticSrc/js/gdb_test.js
+++ assets/staticSrc/js/gdb_test.js
@@ -1,14 +1,18 @@
 const RDFMimeType = require('graphdb/lib/http/rdf-mime-type');
-const SparqlJsonResultParser = require('graphdb/lib/parser/sparql-json-result-parser');
-const SparqlXmlResultParser = require('graphdb/lib/parser/sparql-xml-result-parser');
-const TurtleParser = require('graphdb/lib/parser/turtle-parser');
-
-const query = 'CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}' ;
-//const query = 'SELECT * WHERE { ?s ?p ?o }' ;
+//const SparqlJsonResultParser = require('graphdb/lib/parser/sparql-json-result-parser');
+//const SparqlXmlResultParser = require('graphdb/lib/parser/sparql-xml-result-parser');
+const {TurtleParser} = require('graphdb').parser ;
 
 //RDF repository client
 const {RDFRepositoryClient, RepositoryClientConfig} = require('graphdb').repository ;
 const {GetQueryPayload, QueryType} = require('graphdb').query ;
+
+//const query = 'CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}' ;
+const query = 'SELECT * WHERE { ?s ?p ?o }' ;
+//const mimeType = RDFMimeType.RDF_JSON ;
+const mimeType = RDFMimeType.SPARQL_RESULTS_JSON ;
+//const queryType = QueryType.CONSTRUCT ;
+const queryType = QueryType.SELECT ;
 
 const endpoint = 'http://localhost:7200' ;
 const readTimeout = 30000 ;
@@ -24,17 +28,20 @@ console.log('repository: ', repository.repositoryClientConfig) ;
 
 //RDF query
 //repository.registerParser(new TurtleParser()) ;
+//repository.registerParser(new SparqlJsonResultParser()) ;
+
 
 const payload = new GetQueryPayload()
-    .setResponseType(RDFMimeType.TURTLE)
+    .setResponseType(mimeType)
     .setQuery(query)
-    .setQueryType(QueryType.CONSTRUCT)
+    .setQueryType(queryType)
     .setLimit(3) ;
 
     return repository.query(payload).then((stream) => {
         stream.on('data', (data) => {
             // handle data
             console.log('data: ', data.toString()) ;
+            //console.log('data: ', data) ;
         });
         stream.on('end', () => {
           // handle end of the stream
