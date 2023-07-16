1495dde79802e592c80ba45a1914e7ab0c72c73e
rh <richard.hoermann@inode.at>
2023-07-16 15:17:37 +0200

    build graphdb js

--- assets/staticSrc/js/gdb_test.js
+++ assets/staticSrc/js/gdb_test.js
@@ -1,5 +1,14 @@
+const RDFMimeType = require('graphdb/lib/http/rdf-mime-type');
+const SparqlJsonResultParser = require('graphdb/lib/parser/sparql-json-result-parser');
+const SparqlXmlResultParser = require('graphdb/lib/parser/sparql-xml-result-parser');
+const TurtleParser = require('graphdb/lib/parser/turtle-parser');
+
+const query = 'CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}' ;
+//const query = 'SELECT * WHERE { ?s ?p ?o }' ;
+
 //RDF repository client
 const {RDFRepositoryClient, RepositoryClientConfig} = require('graphdb').repository ;
+const {GetQueryPayload, QueryType} = require('graphdb').query ;
 
 const endpoint = 'http://localhost:7200' ;
 const readTimeout = 30000 ;
@@ -12,3 +21,24 @@ const config = new RepositoryClientConfig(endpoint)
     
 const repository = new RDFRepositoryClient(config) ;
 console.log('repository: ', repository.repositoryClientConfig) ;
+
+//RDF query
+//repository.registerParser(new TurtleParser()) ;
+
+const payload = new GetQueryPayload()
+    .setResponseType(RDFMimeType.TURTLE)
+    .setQuery(query)
+    .setQueryType(QueryType.CONSTRUCT)
+    .setLimit(3) ;
+
+    return repository.query(payload).then((stream) => {
+        stream.on('data', (data) => {
+            // handle data
+            console.log('data: ', data.toString()) ;
+        });
+        stream.on('end', () => {
+          // handle end of the stream
+        });
+      });
+
+
