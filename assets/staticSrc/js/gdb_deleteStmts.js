a4e63432a7d2b02a652f028746640780c085677d
rh <richard.hoermann@inode.at>
2023-08-06 23:10:06 +0200

    build ttl to html

--- assets/staticSrc/js/gdb_deleteStmts.js
+++ assets/staticSrc/js/gdb_deleteStmts.js
@@ -1,36 +1,33 @@
 const fs = require('fs') ;
-//RDF repository client
-const {RDFRepositoryClient, RepositoryClientConfig} = require('graphdb').repository ;
-const {GetQueryPayload, QueryType} = require('graphdb').query ;
-//const RDFMimeType = require('graphdb/lib/http/rdf-mime-type') ;
+const { EnapsoGraphDBClient } = require('@innotrade/enapso-graphdb-client') ;
+const { EnapsoGraphDBAdmin } = require('@innotrade/enapso-graphdb-admin');
+var connectFlag = false ;
 
-const endpoint = process.env.endpoint ; // 'http://localhost:7200'
-const repo_name = process.env.repo_name ; // 'kfngoe_test'
+const endpoint = process.env.endpoint ;
+const repo_name = process.env.repo_name ;
 
 const path_rq = process.env.path_rq ; // 'assets/staticSrc/sparql/'
 const file_rq = process.env.file_rq ; // 'annoPerson_1.rq'
 const ext_rq = process.env.ext_rq ; // '.rq'
 const filepath_rq = path_rq + file_rq + ext_rq ;
 
-const readTimeout = 30000 ;
-const writeTimeout = 30000 ;
-const config = new RepositoryClientConfig(endpoint)
-    .setEndpoints([ endpoint + '/repositories/' + repo_name])
-    //.setEndpoint('http://localhost:7200/repositories/kfngoe_test')
-    .setReadTimeout(readTimeout)
-    .setWriteTimeout(writeTimeout) ;    
-    
-const repository = new RDFRepositoryClient(config) ;
-console.log('repository: ', repository.repositoryClientConfig) ;
+let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
+    baseURL: endpoint,
+    repository: repo_name,    
+    triplestore: 'graphdb',
+    version: 10,
+    apiType: 'RDF4J'
+}) ;
 
-//RDF delete query
+//graphdb delete statements
 (async () => {
     const query = fs.readFileSync(filepath_rq, 'utf8');
     console.log('query data read: ', query.length, ' bytes') ;
-    console.log('query: ', query) ;    
-        
-    let result = await repository.deleteStatements(query).catch((err) => {
-        console.log(err);
-    }) ;    
-    console.log('result: ', result) ;
-})() ;
+
+    let result = await graphDBEndpoint
+        .update( query )
+        .catch((err) => {
+            console.log(err);
+        }) ;
+    console.log('Delete the class:\n' + JSON.stringify(result, null, 2)) ;
+  })() ;
--- build_ttl2html.sh
+++ build_ttl2html.sh
@@ -4,16 +4,17 @@ ENDPOINT='http://localhost:7200'
 REPO_NAME='kfngoe_test'
 
 echo "clear graphdb repo"
-#endpoint=$ENDPOINT repo_name=$REPO_NAME node assets/staticSrc/js/gdb_clearRepo.js
+endpoint=$ENDPOINT repo_name=$REPO_NAME node assets/staticSrc/js/gdb_clearRepo.js
 
 PATH_IN='data/ttl/text/'
 FILENAME_IN='Bae_TB_8'
 EXTENSION_IN='.ttl'
 
-echo "import file to graphdb repo"
-#endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
-
 MIME_TYPE='text/turtle'
+
+echo "import file to graphdb repo"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
+
 QUERY_TYPE='CONSTRUCT'
 
 PATH_RQ='assets/staticSrc/sparql/'
@@ -26,23 +27,96 @@ EXTENSION_OUT='.ttl'
 
 echo "query graphdb repo"
 echo "group keys from TEI statements"
-#endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ mime_type=$MIME_TYPE query_type=$QUERY_TYPE path_out=$PATH_OUT file_out=$FILENAME_OUT ext_out=$EXTENSION_OUT node assets/staticSrc/js/gdb_queryRepo.js
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ mime_type=$MIME_TYPE query_type=$QUERY_TYPE path_out=$PATH_OUT file_out=$FILENAME_OUT ext_out=$EXTENSION_OUT node assets/staticSrc/js/gdb_queryRepo.js
 
 PATH_IN='./data/ttl/annotation/anno_web/instance/'
 FILENAME_IN='annoPersoni_1'
 EXTENSION_IN='.ttl'
 
 echo "import file to graphdb repo"
-#endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
+echo "import annoPersoni_1"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
 
 FILENAME_RQ='annoPerson_2'
 FILENAME_OUT='annoPersoni_2'
 
 echo "query graphdb repo"
 echo "generate resource IDs for grouped keys"
-#endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ mime_type=$MIME_TYPE query_type=$QUERY_TYPE path_out=$PATH_OUT file_out=$FILENAME_OUT ext_out=$EXTENSION_OUT node assets/staticSrc/js/gdb_queryRepo.js
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ mime_type=$MIME_TYPE query_type=$QUERY_TYPE path_out=$PATH_OUT file_out=$FILENAME_OUT ext_out=$EXTENSION_OUT node assets/staticSrc/js/gdb_queryRepo.js
 
 FILENAME_RQ='del_annoPerson_1'
 
 echo "delete graphdb statements"
-endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ node assets/staticSrc/js/gdb_deleteStmts.js   
+echo "delete annoPerson_1"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ node assets/staticSrc/js/gdb_deleteStmts.js
+
+PATH_IN='./data/ttl/annotation/anno_web/instance/'
+FILENAME_IN='annoPersoni_2'
+EXTENSION_IN='.ttl'
+
+echo "import file to graphdb repo"
+echo "import annoPersoni_2"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
+
+FILENAME_RQ='annoPerson_3'
+FILENAME_OUT='annoPersoni_3'
+
+echo "query graphdb repo"
+echo "build targets of annotations"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ mime_type=$MIME_TYPE query_type=$QUERY_TYPE path_out=$PATH_OUT file_out=$FILENAME_OUT ext_out=$EXTENSION_OUT node assets/staticSrc/js/gdb_queryRepo.js
+
+FILENAME_RQ='del_annoPerson_2'
+
+echo "delete graphdb statements"
+echo "delete annoPerson_2"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ node assets/staticSrc/js/gdb_deleteStmts.js
+
+PATH_IN='./data/ttl/annotation/anno_web/instance/'
+FILENAME_IN='annoPersoni_3'
+EXTENSION_IN='.ttl'
+
+echo "import file to graphdb repo"
+echo "import annoPersoni_3"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
+
+PATH_IN='./data/ttl/annotation/person/instance/'
+FILENAME_IN='personi'
+EXTENSION_IN='.ttl'
+
+echo "import file to graphdb repo"
+echo "import personi"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
+
+FILENAME_RQ='annoPerson_4'
+FILENAME_OUT='annoPersoni'
+
+echo "query graphdb repo"
+echo "build bodies of annotations"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ mime_type=$MIME_TYPE query_type=$QUERY_TYPE path_out=$PATH_OUT file_out=$FILENAME_OUT ext_out=$EXTENSION_OUT node assets/staticSrc/js/gdb_queryRepo.js
+
+FILENAME_RQ='del_annoPerson_3'
+
+echo "delete graphdb statements"
+echo "delete annoPerson_3"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ node assets/staticSrc/js/gdb_deleteStmts.js
+
+PATH_IN='./data/ttl/annotation/anno_web/instance/'
+FILENAME_IN='annoPersoni'
+EXTENSION_IN='.ttl'
+
+echo "import file to graphdb repo"
+echo "import annoPersoni"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_in=$PATH_IN file_in=$FILENAME_IN ext_in=$EXTENSION_IN mime_type=$MIME_TYPE node assets/staticSrc/js/gdb_importFile.js
+
+FILENAME_RQ='person'
+
+PATH_OUT='./data/json/register/'
+FILENAME_OUT='person'
+EXTENSION_OUT='.json'
+
+MIME_TYPE='application/sparql-results+json'
+QUERY_TYPE='SELECT'
+
+echo "query graphdb repo"
+echo "build register person json"
+endpoint=$ENDPOINT repo_name=$REPO_NAME path_rq=$PATH_RQ file_rq=$FILENAME_RQ ext_rq=$EXTENSION_RQ mime_type=$MIME_TYPE query_type=$QUERY_TYPE path_out=$PATH_OUT file_out=$FILENAME_OUT ext_out=$EXTENSION_OUT node assets/staticSrc/js/gdb_queryRepo.js
