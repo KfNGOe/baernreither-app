d16b08b63cbed374e817459b02e461dffbbf6f21
rh <richard.hoermann@inode.at>
2023-09-09 16:46:46 +0200

    build place ttl template

---
+++ assets/staticSrc/js/build_place_json2ttl.js
@@ -0,0 +1,198 @@
+//convert json to turtle for person register
+const { groupBy } = require("core-js/actual/array/group-by") ;
+// Importing the jsdom module
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const date = new Date() ;
+console.log(date);
+
+var prefInstance = "kfngoei:" ;
+var prefOntology = "kfngoeo:" ;
+
+const dateMod = '"' + date.toISOString() + '"' ;
+const lic = '<http://creativecommons.org/publicdomain/zero/1.0/>' ;
+const src_gnd = '<http://d-nb.info/standards/elementset/gnd#>' ;
+const src_gn = '<https://www.geonames.org/ontology#>' ;
+
+const LF = "\n" ;
+const TAB = "\t" ;
+const COMMA = ", " ;
+const DOT = " ." ;
+const SEMICOLON = " ;" ;
+const SPACE = " " ;
+const QUOT = '"' ;
+const SQBRACKET_OPEN = "[" ;
+const SQBRACKET_CLOSE = "]" ;
+const BN = "_:" ;
+
+var label_en = '""' ;
+var label_de = '""' ;
+var prefName = '""' ;
+var bio = '""' ;
+var propVal_gnd = '""' ;
+var propVal_cc = '""' ;
+
+const prefix =   "@prefix kfngoeo: <https://github.com/KfNGOe/kfngoeo#> ." + LF 
+               + "@prefix kfngoei: <https://github.com/KfNGOe/kfngoei/> ." + LF               
+               + "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ." + LF
+               + "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ." + LF
+               + "@prefix gndo: <http://d-nb.info/standards/elementset/gnd#> ." + LF
+               + "@prefix	gn: <https://www.geonames.org/ontology#> ." + LF
+            ;
+    
+
+var ttl = prefix + LF + LF ;
+
+//convert special characters to html encoding
+function convertChar2Html(str) {
+   return str.replace(/'/g,"&apos;")       
+       .replace(/"/g, "&quot;")
+   ;
+}
+
+function checkKeys(obj) {
+   //test if key exists
+   if ('A' in obj) {      
+   } else {      
+      obj.A = '' ;
+   }
+   if ('B' in obj) {      
+   } else {
+      obj.B = '' ;
+   }
+   if ('C' in obj) {
+   } else {
+      obj.C = '' ;
+   }
+   if ('D' in obj) {
+   } else {
+      obj.D = '' ;
+   }
+   if ('E' in obj) {
+   } else {
+      obj.E = '' ;
+   }
+   if ('F' in obj) {      
+      obj.F = convertChar2Html(obj.F) ;
+   } else {      
+      obj.F = '' ;
+   }
+   if ('G' in obj) {
+      obj.G = obj.G.replace('https://d-nb.info/gnd/', '') ;
+   } else {
+      obj.G = '' ;
+   }
+   if ('H' in obj) {
+   } else {
+      obj.H = '' ;
+   }
+   if ('I' in obj) {
+   } else {
+      obj.I = '' ;
+   }
+   if ('J' in obj) {
+   } else {
+      obj.J = '' ;
+   }
+}
+
+function getPrefName(obj) {
+   //build preferred name
+   if(obj.A != '') {
+      if (obj.C != '' || obj.B != '') {
+         if (obj.C != '') {
+            if (obj.B != '') {
+               prefName = obj.A + COMMA + obj.C + SPACE + obj.B ;            
+            } else {
+               prefName = obj.A + COMMA + obj.C ;
+            }             
+         } else {
+            prefName = obj.A + SPACE + obj.B ;
+         }
+      }
+      else {
+         prefName = obj.A ;       
+      }
+   } else {
+      if (obj.C != '' || obj.B != '') {
+         if (obj.C != '') {
+            if (obj.B != '') {
+               prefName = obj.C + SPACE + obj.B ;            
+            } else {
+               prefName = obj.C ;
+            }             
+         } else {
+            prefName = obj.B ;
+         }
+      }
+      else {
+         prefName = '' ;       
+      }
+   }
+}
+
+function getObject(obj) {
+   let length = Object.keys(obj).length ;
+   console.log('object length =', length) ;
+   resourceIri = prefInstance + uuidv4() ;
+   //console.log( 'resourceIri = ', resourceIri ) ;
+   checkKeys(obj) ;
+   getPrefName(obj) ;
+   
+   var ttl_tmp = '' ;
+   ttl_tmp = ttl_tmp + resourceIri + LF ;
+   ttl_tmp = ttl_tmp + TAB + 'a' + SPACE + prefOntology + 'PlaceOrGeographicName' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'modified' + SPACE + dateMod + '^^xsd:dateTime' + SEMICOLON + LF ; 
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'license' + SPACE + lic + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'source' + SPACE + src_gn + COMMA + SPACE + src_gnd + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'preferredNameForThePlaceOrGeographicName' + SPACE + QUOT + prefName + QUOT + '^^xsd:string' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'gender' + SPACE + gender + SEMICOLON + LF ;  
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'dateOfBirth' + SPACE + QUOT + obj.D + QUOT + '^^xsd:date' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'dateOfDeath' + SPACE + QUOT + obj.E + QUOT + '^^xsd:date' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'biographicalOrHistoricalInformation' + SPACE + QUOT + obj.F + QUOT + '^^xsd:string' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'identifier' + SPACE + SQBRACKET_OPEN + LF ;
+   ttl_tmp = ttl_tmp + TAB + TAB + 'a' + SPACE + prefOntology + 'PropertyValue' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + TAB + prefOntology + 'propertyID "GND"^^xsd:string' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + TAB + prefOntology + 'value' + SPACE + QUOT + obj.G + QUOT + '^^xsd:string' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + SQBRACKET_CLOSE + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + prefOntology + 'identifier' + SPACE + SQBRACKET_OPEN + LF ;
+   ttl_tmp = ttl_tmp + TAB + TAB + 'a' + SPACE + prefOntology + 'PropertyValue' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + TAB + prefOntology + 'propertyID "CamelCase"^^xsd:string' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + TAB + prefOntology + 'value' + SPACE + QUOT + obj.H + QUOT + '^^xsd:string' + SEMICOLON + LF ;
+   ttl_tmp = ttl_tmp + TAB + SQBRACKET_CLOSE + ' .' + LF + LF ;
+   ttl = ttl + ttl_tmp ;
+   //console.log('ttl = ', ttl) ;      
+} ; 
+
+function getArray(arr) {
+   let length = arr.length ;   
+   console.log('array length =', length) ;
+   arr.forEach((item, index, array) => {
+      if (typeof item === 'object') {
+         console.log('index = ', index) ;          
+         getObject(item) ;
+      }
+   }) ;
+   //console.log('result: ',arr) ;   
+} ;
+
+var json = fs.readFileSync('./data/json_xlsx/person_xlsx.json', 'utf8');
+console.log('json data read: ', json.length, ' bytes')
+
+var jsonJS = JSON.parse(json) ;
+var persons = jsonJS.Tabelle1 ;
+//console.log('jsonJS: ', persons[0]) ;
+getArray(persons) ;
+//console.log('ttl = ', ttl) ;
+
+//write ttl file
+//filepath = path_out_ttl + filename + ext_ttl ;
+//console.log(filepath);
+fs.writeFileSync('./data/ttl/annotation/person/instance/personi.ttl', ttl ) ;
+console.log('ttl data written: ', ttl.length  , ' bytes')
+
+//group by gnd
+//var personsGND = persons.groupBy(person => {
+//    return person.G ;
+//}) ;
+//console.log('personsGND: ', personsGND) ;
