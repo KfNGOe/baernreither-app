073aa67ce65f6e547e725d548853acea9e81499e
rh <richard.hoermann@inode.at>
2023-09-11 06:10:00 +0200

    build xlsx to json

--- assets/staticSrc/js/build_xlsx2json.js
+++ assets/staticSrc/js/build_xlsx2json.js
@@ -2,15 +2,15 @@
 const fs = require('fs');
 const excelToJson = require('convert-excel-to-json');
 
-const path_in = process.env.path ;
-const file_in = process.env.file; 
-const ext_in = process.env.ext ;
+const path_in = process.env.path_in ;
+const file_in = process.env.file_in; 
+const ext_in = process.env.ext_in ;
 const filepath_in = path_in + file_in + ext_in ;
 console.log(filepath_in);
 
-const path_out = process.env.path ;
-const file_out = process.env.file; 
-const ext_out = process.env.ext ;
+const path_out = process.env.path_out ;
+const file_out = process.env.file_out; 
+const ext_out = process.env.ext_out ;
 const filepath_out = path_out + file_out + ext_out ;
 console.log(filepath_out);
 
--- build_xlsx2json.sh
+++ build_xlsx2json.sh
@@ -1,14 +1,16 @@
-echo "build excel to json"
+echo "convert excel to json"
 
-PATH_XLSX='./data/json_xlsx/'
+PATH_XLSX='./data/xlsx/'
 PATH_JSON_XLSX='./data/json_xlsx/'
 
 #FILENAME=$name
-FILENAME_IN='Baernreither_Personenregister_2023'
-FILENAME_OUT='person_xlsx'
+#FILENAME_IN='Baernreither_Personenregister_2023'
+FILENAME_IN='Baernreither_Ortsregister_2023'
+#FILENAME_OUT='person_xlsx'
+FILENAME_OUT='place_xlsx'
 
 EXTENSION_XLSX='.xlsx'
 EXTENSION_JSON='.json'
 
-echo "normalize whitespace"
-path_in=$PATH_XLSX file_in=$FILENAME_IN ext_in=$EXTENSION_XLSX path_out=$PATH_JSON_XLSX file_out=$FILENAME_OUT ext=$EXTENSION_JSON node assets/staticSrc/js/build_xlsx2json.js
+echo "build xlsx to json"
+path_in=$PATH_XLSX file_in=$FILENAME_IN ext_in=$EXTENSION_XLSX path_out=$PATH_JSON_XLSX file_out=$FILENAME_OUT ext_out=$EXTENSION_JSON node assets/staticSrc/js/build_xlsx2json.js
