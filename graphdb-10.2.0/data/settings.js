53b7cba97fa282f304ef42f4396380560cb2dfdb
rh <richard.hoermann@inode.at>
2023-10-11 20:09:44 +0200

    build anno text compare connect annos

--- graphdb-10.2.0/data/settings.js
+++ graphdb-10.2.0/data/settings.js
@@ -12,29 +12,6 @@
     }
   },
   "import.local" : {
-    "kfngoe_test;;Bae_MF_6-2.ttl" : {
-      "name" : "Bae_MF_6-2.ttl",
-      "status" : "NONE",
-      "message" : "",
-      "context" : "",
-      "replaceGraphs" : [ ],
-      "baseURI" : null,
-      "forceSerial" : false,
-      "type" : "file",
-      "format" : null,
-      "data" : "03867df3-c8ed-425f-8573-36976454311a",
-      "timestamp" : 1697016225531,
-      "parserSettings" : {
-        "preserveBNodeIds" : false,
-        "failOnUnknownDataTypes" : false,
-        "verifyDataTypeValues" : false,
-        "normalizeDataTypeValues" : false,
-        "failOnUnknownLanguageTags" : false,
-        "verifyLanguageTags" : true,
-        "normalizeLanguageTags" : false,
-        "stopOnError" : true
-      }
-    },
     "kfngoe_test;;Bae_TB_8.ttl" : {
       "name" : "Bae_TB_8.ttl",
       "status" : "DONE",
@@ -47,6 +24,75 @@
       "format" : null,
       "data" : "49821255-3eb6-4bf6-b2f3-b21433b01525",
       "timestamp" : 1697028652922,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "kfngoe_test;;Bae_MF_6-2.ttl" : {
+      "name" : "Bae_MF_6-2.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/Bae_MF_6-2.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "9a394494-2aa8-48c2-8d9e-c1c4467fd3a0",
+      "timestamp" : 1697035596780,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "kfngoe_test;;annoTextCompi_2.ttl" : {
+      "name" : "annoTextCompi_2.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/annoTextCompi_2.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "81010f8c-de1a-438f-ab34-2ce28e23684c",
+      "timestamp" : 1697035648569,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "kfngoe_test;;annoTextCompi_1.ttl" : {
+      "name" : "annoTextCompi_1.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/annoTextCompi_1.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "5e65474a-a4e4-463a-b9be-f65bec2a7203",
+      "timestamp" : 1697035648570,
       "parserSettings" : {
         "preserveBNodeIds" : false,
         "failOnUnknownDataTypes" : false,
