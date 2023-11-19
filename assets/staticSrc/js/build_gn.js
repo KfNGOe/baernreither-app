6d05883b3d87611ba5663de4038cac980e22dd1c
rh <richard.hoermann@inode.at>
2023-11-19 19:11:15 +0100

    build geonames json

--- assets/staticSrc/js/build_gn.js
+++ assets/staticSrc/js/build_gn.js
@@ -2,19 +2,21 @@ const axios = require('axios') ;
 const fs = require('fs') ;
 var convert = require('xml-js');
 
-var gnSets = "" ;
+var gnSets = [] ;
+var gnSets_json = {} ;
+var gnSets_str = "" ;
 var gnSet = "" ;
 var gnUrl = "" ;
-var gnUrls = [] ;
+
+var gndUrl = "" ;
 
 async function getGnData(gnUrl) {
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: gnUrl,
+        url: gnUrl, //"https://sws.geonames.org/3020251/about.rdf"
         headers: { }
-    };
-    
+    } ;    
     const response = await axios.request(config)
     .catch(error => {
         console.log(error) ;
@@ -22,32 +24,19 @@ async function getGnData(gnUrl) {
     return response ;
 }
 
-async function getObject(obj) {
-    let length = Object.keys(obj).length ;
-    console.log('object length =', length) ;    
-    //console.log( 'resourceIri = ', resourceIri ) ;
-    if ('G' in obj && obj.G.includes('https')) {        
-        gndUrl = obj.G.concat('/about/lds') ;
-        console.log('gndUrl = ', gndUrl) ;
-        gndUrls.push(gndUrl) ;      
-        //gnd_api('https://d-nb.info/gnd/119148331/about/lds') ;      
-     }
-    
-  } ;
-  
-  function getArray(arr) {
-    let length = arr.length ;   
-    console.log('array length =', length) ;
-    arr.forEach(async (item, index, array) => {
-       if (typeof item === 'object') {
-          console.log('index = ', index) ;          
-          getObject(item) ;
-       }
-    }) ;
-    //console.log('result: ',arr) ;   
-  } ;
-
-//geoname_api("https://sws.geonames.org/3020251/about.rdf") ;
+async function getGNDData(gndUrl) {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: gndUrl,
+      headers: { }
+    };  
+    const response = await axios.request(config)
+    .catch(error => {
+        console.log(error) ;
+    }) ;    
+    return response ;
+  }
 
 (async () => {
 
@@ -56,41 +45,57 @@ async function getObject(obj) {
     var jsonJS = JSON.parse(json) ;
     var places = jsonJS.Tabelle1 ;
   
-    //getArray(places) ;
-    //console.log('gnUrls: ', gnUrls) ;
-  
     console.log('start gn api calls') ;
   
     for (const item of places) {
       console.log('item = ', item) ;
-      if (item.E.includes('https')) {        
-        gnUrl = item.E ;
-        gnUrl = gnUrl.replace('https://www.geonames.org/', 'https://sws.geonames.org/') ;
-        gnUrl= gnUrl.substring(0, gnUrl.lastIndexOf('/')) ;        
-        gnUrl = gnUrl.concat('/about.rdf') ;   
-        console.log('gnUrl = ', gnUrl) ;
+      //check if item.E exists
+        if (item.E && item.E.includes('https')) {
+            if(item.E.includes('geonames')) {
+                gnUrl = item.E ;
+                gnUrl = gnUrl.replace('https://www.geonames.org/', 'https://sws.geonames.org/') ;
+                gnUrl= gnUrl.substring(0, gnUrl.lastIndexOf('/')) ;        
+                gnUrl = gnUrl.concat('/about.rdf') ;   
+                console.log('gnUrl = ', gnUrl) ;
 
-        await getGnData(gnUrl).then(response => {
-            //console.log('response.data: ', response.data) ;
-            xml = response.data ;
-            //gnSets = gndSets + gndSet ;
-          })
-          //console.log('xml: ', xml) ;
-          //convert xml to json object
-          json = convert.xml2json(xml, {compact: false, spaces: 2}) ;
-          let arr_json = json.declaration ;          
-          
-          fs.writeFileSync('./data/json_xmlId/test.json', arr_json ) ;
-          console.log('json data written: ', arr_json.length  , ' bytes') ;
-      }
-      //var result = convert.xml2json(xml, {compact: false, spaces: 2});      
-    }  
-    
-    console.log('end gnd api calls') ;
-    
-    //console.log('gndSets: ', gndSets) ;  
-    
-    fs.writeFileSync('./data/ttl/annotation/person/instance/gnd/gnd.ttl', gndSets ) ;
-    console.log('ttl data written: ', gndSets.length  , ' bytes')
-    
+                await getGnData(gnUrl).then(response => {                    
+                    xml = response.data ;                    
+                })
+                js = convert.xml2js(xml, {compact: false, spaces: 2}) ;
+                let arr_js = js.elements[0].elements[0].elements ;
+                let wgs_lat = arr_js.find(item => item.name === 'wgs84_pos:lat').elements[0].text ;
+                let wgs_long = arr_js.find(item => item.name === 'wgs84_pos:long').elements[0].text ;
+                console.log('wgs_lat: ', wgs_lat) ;
+                console.log('wgs_long: ', wgs_long) ;
+                item.Lat = wgs_lat ;
+                item.Long = wgs_long ;
+                gnSet = item ;
+                gnSets.push(gnSet) ;
+                console.log('gnSets: ', JSON.stringify(gnSets)) ;
+            } else {
+                if(item.E.includes('gnd')) {                    
+                    gnSet = item ;
+                    gnSets.push(gnSet) ;
+                    /*gndUrl = item.E ;
+                    gndUrl = gndUrl.concat('/about/lds') ;
+                    console.log('gndUrl = ', gndUrl) ;
+
+                    await getGndData(gndUrl).then(response => {
+                        //console.log('response.data: ', response.data) ;
+                        xml = response.data ;
+                        //gnSets = gndSets + gndSet ;
+                    })*/
+                }
+            }
+        } else {            
+            //add item
+              gnSet = item ;
+              gnSets.push(gnSet) ;
+        }      
+    }          
+    console.log('end api calls') ;
+    gnSets_json.Tabelle1 = gnSets ;
+    gnSets_str = JSON.stringify(gnSets_json) ;
+    fs.writeFileSync('./data/json/register/register_place_temp.json', gnSets_str) ;
+    console.log('json data write: ', gnSets_str.length, ' bytes') ;
   })() ;
