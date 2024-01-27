57827ea081456c9a2239d58f6ba66ca0dd5bcbe5
rh <richard.hoermann@inode.at>
2024-01-27 17:53:47 +0100

    build dipl text html TB 8

--- assets/staticSrc/js/build_textDipl_json2html.js
+++ assets/staticSrc/js/build_textDipl_json2html.js
@@ -84,90 +84,81 @@ function buildDiplText(obj, obj_1) {
       //console.log('key = ', key) ;
       if (posStr2Nr(key) >= posStr2Nr(pos_body)) {
          //console.log('key = ', key) ;
-         groupedByPos[key].forEach((item, index, array) => {
-               //console.log('item = ', item) ;
-               switch(item.type.value) {
-                  case 'https://github.com/KfNGOe/kfngoeo#StartTag':
-                     switch(item.name.value) {
-                        case 'http://www.tei-c.org/ns/1.0/p': 
-                           html_str = html_str.concat('<p>') ;                              
-                           break ;
-                        case 'http://www.tei-c.org/ns/1.0/anchor':
-                           html_str = html_str.concat('<a href="#" id="comp_' + generateId(item) + '" style="display: none"><img src="images/anchor.png" title="click"></a>') ;                           
-                           /*
-                           if (appStack.start == '' || appStack.end == '') {
-                              let pos_arr = groupedByAnchApp[posStr2Nr(item.pos.value).toString()] ;
-                              if (pos_arr !== undefined) {
-                                 appStack.start = pos_arr[0].start.value ;
-                                 appStack.end = pos_arr[0].end.value ;
-                                 html_str = html_str.concat('<span class="comp-passage" style="display: none">') ;
-                              } else {
-                                 console.log('pos anchor not in annoTextComp : ', posStr2Nr(item.pos.value).toString()) ;
-                              }                              
-                           }*/
-                           break ;
-                        case 'http://www.tei-c.org/ns/1.0/app':                            
-                           break ;
-                        default:
-                           break ;
-                     }                        
-                     break ;
-                  case 'https://github.com/KfNGOe/kfngoeo#EndTag':
-                     switch(item.name.value) {
-                        case 'http://www.tei-c.org/ns/1.0/p': 
-                           html_str = html_str.concat('</p>') ;                              
-                           break ;
-                        default:
-                           break ;
-                     }
-                     break ;
-                  case 'https://github.com/KfNGOe/kfngoeo#Text':
-                     //check if text is between anchor and app pos                        
-                     let item_hit = obj_1.results.bindings.find((item_1, index_1, array_1) => {
-                        //console.log('item_1 = ', item_1) ;
-                        //check if same text source
-                        let flag_source = (item.pos.value.includes(item_1.source_target.value)) ? true : false ;                        
-                        return (+item_1.start.value < posStr2Nr(item.pos.value)) && (posStr2Nr(item.pos.value) < +item_1.end.value && flag_source) ;
-                     }) ;
-                     if (item_hit !== undefined) {
-                        //console.log('item_hit = ', item_hit) ;
-                        html_str = html_str.concat('<span class="comp-passage" id="comp_' + generateId(item) + '">') ;                           
-                        html_str = html_str.concat(item.cont.value) ;
-                        html_str = html_str.concat('</span>') ;
-                     } else {                        
-                        //check if text is between app pos
-                        let pos_tmp = posNr2Str(posStr2Nr(item.pos.value) - 2) ;
-                        let pos_arr = groupedByPos[pos_tmp] ;                        
-                        if (pos_arr[0].name !== undefined && pos_arr[0].name.value == 'http://www.tei-c.org/ns/1.0/app' 
-                           && pos_arr[0].type.value == 'https://github.com/KfNGOe/kfngoeo#StartTag') {
-                              html_str = html_str.concat('<span id="comp_' + generateId(item) + '" style="display: none">') ;
-                              html_str = html_str.concat(item.cont.value) ;
-                              html_str = html_str.concat('</span>') ;
-                        } else {
-                           html_str = html_str.concat('<span id="text_' + generateId(item) + '">') ;
-                           html_str = html_str.concat(item.cont.value) ;
-                           html_str = html_str.concat('</span>') ;                           
-                        }
-                     }
-                     break ;                     
-                  default:
-                     break ;
-               }
-/*
-               if (item.name !== undefined) {
+         let item = groupedByPos[key][0] ;
+         //console.log('item = ', item) ;
+         switch(item.type.value) {
+            case 'https://github.com/KfNGOe/kfngoeo#StartTag':
                switch(item.name.value) {
                   case 'http://www.tei-c.org/ns/1.0/p': 
-                     if (item.type.value == 'https://github.com/KfNGOe/kfngoeo#StartTag') {
-                        html_str = html_str.concat('<p>') ;
-                     } else if (item.type.value == 'https://github.com/KfNGOe/kfngoeo#EndTag') {
-                        html_str = html_str.concat('</p>') ;
-                     }
+                     html_str = html_str.concat('<p>') ;                              
+                     break ;
+                  case 'http://www.tei-c.org/ns/1.0/anchor':
+                     html_str = html_str.concat('<a href="#" id="comp_' + generateId(item) + '"><img src="images/anchor.png" title="click"></a>') ;                           
+                     /*
+                     if (appStack.start == '' || appStack.end == '') {
+                        let pos_arr = groupedByAnchApp[posStr2Nr(item.pos.value).toString()] ;
+                        if (pos_arr !== undefined) {
+                           appStack.start = pos_arr[0].start.value ;
+                           appStack.end = pos_arr[0].end.value ;
+                           html_str = html_str.concat('<span class="comp-passage" style="display: none">') ;
+                        } else {
+                           console.log('pos anchor not in annoTextComp : ', posStr2Nr(item.pos.value).toString()) ;
+                        }                              
+                     }*/
+                     break ;
+                  case 'http://www.tei-c.org/ns/1.0/app':
+                     break ;
+                  case 'http://www.tei-c.org/ns/1.0/note':
+                     html_str = html_str.concat('<span id="note_' + generateId(item) + '" style="display: none">') ;
+                     break ;
+                  default:
+                     break ;
+               }                        
+               break ;
+            case 'https://github.com/KfNGOe/kfngoeo#EndTag':
+               switch(item.name.value) {
+                  case 'http://www.tei-c.org/ns/1.0/p': 
+                     html_str = html_str.concat('</p>') ;
+                     break ;
+                  case 'http://www.tei-c.org/ns/1.0/note':
+                     html_str = html_str.concat('</span>') ;
                      break ;
                   default:
                      break ;
                }
-               }*/ 
-         }) ;
+               break ;
+            case 'https://github.com/KfNGOe/kfngoeo#Text':
+               //check if text is between anchor and app pos                        
+               let item_hit = obj_1.results.bindings.find((item_1, index_1, array_1) => {
+                  //console.log('item_1 = ', item_1) ;
+                  //check if same text source
+                  let flag_source = (item.pos.value.includes(item_1.source_target.value)) ? true : false ;                        
+                  return (+item_1.start.value < posStr2Nr(item.pos.value)) && (posStr2Nr(item.pos.value) < +item_1.end.value && flag_source) ;
+               }) ;
+               if (item_hit !== undefined) {
+                  //console.log('item_hit = ', item_hit) ;
+                  html_str = html_str.concat('<span class="comp-passage" id="comp_' + generateId(item) + '">') ;                           
+                  html_str = html_str.concat(item.cont.value) ;
+                  html_str = html_str.concat('</span>') ;
+               } else {                        
+                  //check if text is between app pos
+                  let pos_tmp = posNr2Str(posStr2Nr(item.pos.value) - 2) ;
+                  let pos_arr = groupedByPos[pos_tmp] ;                        
+                  if (pos_arr[0].name !== undefined && pos_arr[0].name.value == 'http://www.tei-c.org/ns/1.0/app' 
+                     && pos_arr[0].type.value == 'https://github.com/KfNGOe/kfngoeo#StartTag') {
+                        html_str = html_str.concat('<span id="comp_' + generateId(item) + '" style="display: none">') ;
+                        html_str = html_str.concat(item.cont.value) ;
+                        html_str = html_str.concat('</span>') ;
+                  } else {
+                     html_str = html_str.concat('<span id="text_' + generateId(item) + '">') ;
+                     html_str = html_str.concat(item.cont.value) ;
+                     html_str = html_str.concat('</span>') ;                           
+                  }
+               }
+               break ;                     
+            default:
+               break ;
+         }
       }
    }) ;
 } ; 
