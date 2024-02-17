c67602306e0dcc91a7bd8ad90bb86cd3621bef1f
rh <richard.hoermann@inode.at>
2024-02-17 14:35:28 +0100

    build text full json

--- graphdb-10.2.0/data/settings.js
+++ graphdb-10.2.0/data/settings.js
@@ -12,6 +12,29 @@
     }
   },
   "import.local" : {
+    "kfngoe_test;;Bae_MF_6-2.ttl" : {
+      "name" : "Bae_MF_6-2.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in 1s.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/Bae_MF_6-2.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "a34f4b94-578e-484f-a7c8-55c143019b8f",
+      "timestamp" : 1708108234981,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
     "kfngoe_test;;Bae_TB_8.ttl" : {
       "name" : "Bae_TB_8.ttl",
       "status" : "DONE",
@@ -22,100 +45,8 @@
       "forceSerial" : false,
       "type" : "file",
       "format" : null,
-      "data" : "be37c21f-92c0-4665-b081-741b1f3c2864",
-      "timestamp" : 1706020894640,
-      "parserSettings" : {
-        "preserveBNodeIds" : false,
-        "failOnUnknownDataTypes" : false,
-        "verifyDataTypeValues" : false,
-        "normalizeDataTypeValues" : false,
-        "failOnUnknownLanguageTags" : false,
-        "verifyLanguageTags" : true,
-        "normalizeLanguageTags" : false,
-        "stopOnError" : true
-      }
-    },
-    "kfngoe_test;;annoTextCompi_1-2.ttl" : {
-      "name" : "annoTextCompi_1-2.ttl",
-      "status" : "NONE",
-      "message" : "",
-      "context" : "",
-      "replaceGraphs" : [ ],
-      "baseURI" : null,
-      "forceSerial" : false,
-      "type" : "file",
-      "format" : null,
-      "data" : "5742a3cb-9431-407b-831f-6e2c7c7a003f",
-      "timestamp" : 1706215044959,
-      "parserSettings" : {
-        "preserveBNodeIds" : false,
-        "failOnUnknownDataTypes" : false,
-        "verifyDataTypeValues" : false,
-        "normalizeDataTypeValues" : false,
-        "failOnUnknownLanguageTags" : false,
-        "verifyLanguageTags" : true,
-        "normalizeLanguageTags" : false,
-        "stopOnError" : true
-      }
-    },
-    "kfngoe_test;;Bae_MF_6-2.ttl" : {
-      "name" : "Bae_MF_6-2.ttl",
-      "status" : "DONE",
-      "message" : "Imported successfully in less than a second.",
-      "context" : "",
-      "replaceGraphs" : [ ],
-      "baseURI" : "file:/uploaded/generated/Bae_MF_6-2.ttl",
-      "forceSerial" : false,
-      "type" : "file",
-      "format" : null,
-      "data" : "d79d39a0-2d1c-418b-a5fd-b6fd653a8946",
-      "timestamp" : 1706253031048,
-      "parserSettings" : {
-        "preserveBNodeIds" : false,
-        "failOnUnknownDataTypes" : false,
-        "verifyDataTypeValues" : false,
-        "normalizeDataTypeValues" : false,
-        "failOnUnknownLanguageTags" : false,
-        "verifyLanguageTags" : true,
-        "normalizeLanguageTags" : false,
-        "stopOnError" : true
-      }
-    },
-    "kfngoe_test;;register_person.ttl" : {
-      "name" : "register_person.ttl",
-      "status" : "DONE",
-      "message" : "Imported successfully in less than a second.",
-      "context" : "",
-      "replaceGraphs" : [ ],
-      "baseURI" : "file:/uploaded/generated/register_person.ttl",
-      "forceSerial" : false,
-      "type" : "file",
-      "format" : null,
-      "data" : "a3178304-b6aa-41cc-a98e-e6bb486d7ed1",
-      "timestamp" : 1707673380804,
-      "parserSettings" : {
-        "preserveBNodeIds" : false,
-        "failOnUnknownDataTypes" : false,
-        "verifyDataTypeValues" : false,
-        "normalizeDataTypeValues" : false,
-        "failOnUnknownLanguageTags" : false,
-        "verifyLanguageTags" : true,
-        "normalizeLanguageTags" : false,
-        "stopOnError" : true
-      }
-    },
-    "kfngoe_test;;annoPersoni_11.ttl" : {
-      "name" : "annoPersoni_11.ttl",
-      "status" : "DONE",
-      "message" : "Imported successfully in less than a second.",
-      "context" : "",
-      "replaceGraphs" : [ ],
-      "baseURI" : "file:/uploaded/generated/annoPersoni_11.ttl",
-      "forceSerial" : false,
-      "type" : "file",
-      "format" : null,
-      "data" : "737967e5-bba2-4bed-ad5e-8a4c60795773",
-      "timestamp" : 1707682705490,
+      "data" : "1531ca8e-7ef6-4002-9932-0a557216d857",
+      "timestamp" : 1708108235029,
       "parserSettings" : {
         "preserveBNodeIds" : false,
         "failOnUnknownDataTypes" : false,
