92413c8d4896e37ae73773f56fe16364f5cdf653
rH1967 <richard.hoermann@inode.at>
2024-04-08 21:15:42 +0200

    build register json tmp

--- assets/staticSrc/js/build_register_json.js
+++ assets/staticSrc/js/build_register_json.js
@@ -49,14 +49,11 @@ function buildReg(jsonJs_reg_files) {
             "bindings": []
         }
     } ;
-    let places_json = {
+    let orgs_json = {
         "head": {
             "vars": [            
                 "key", 
-                "name", 
-                "name_today",
-                "lat",
-                "long",                
+                "name",                                
                 "pid",
                 "pos"
             ]
@@ -64,7 +61,8 @@ function buildReg(jsonJs_reg_files) {
         "results": {
             "bindings": []
         }
-    } ;
+    } ;    
+
     //read person json files
     let jsonJs_in = jsonJs_reg_files['register_person_text'] ; //person.json
     let jsonJs_in_xlsx = jsonJs_reg_files['register_person_xlsx'] ; //person_xlsx.json    
@@ -193,7 +191,72 @@ function buildReg(jsonJs_reg_files) {
             console.log('warning: no key in place_text.json') ;
         }  
     }) ;   
-} ; 
+} ;
+
+//read org json files
+jsonJs_in = jsonJs_reg_files['register_org_text'] ; //org.json
+jsonJs_in_xlsx = jsonJs_reg_files['register_org_xlsx'] ; //org_xlsx.json
+
+//group by key
+groupedByKey = jsonJs_in.results.bindings.groupBy( item => {  //org_text.json
+    return item.o_key_org.value ;
+}) ;
+groupedByKey_xlsx = jsonJs_in_xlsx.Tabelle1.groupBy( item => {  //org_xlsx.json
+    return item.C ;
+}) ;
+
+//iterate over text file
+jsonJs_in.results.bindings.forEach((item) => { //org.json 
+    console.log('key = ', item.o_key_org.value) ;
+    if (item.o_key_org) {
+        let org = {} ;
+        //let id = generateId() ;        
+        let key = item.o_key_org.value ;
+        let pid = '' ;
+        let pos = item.o_pos_org.value ;
+        //check if pid exists
+        if (!item.o_pid_org) {
+            console.log('warning: no pid '+ key + ' in org_text.json') ;
+            pid = '' ;
+        } else {
+            pid = item.o_pid_org.value ;
+        }
+        //check if key in xlsx exists
+        if (!groupedByKey_xlsx[key]) { //org_xlsx.json
+            console.log('warning: no key ' + key + ' in org_xlsx.json') ;
+            org = {
+                //"id": id,
+                "key": key,
+                "name": '',                    
+                "pid": pid,
+                "pos": pos
+            } ;
+        } else {
+            let item1 = groupedByKey_xlsx[key][0] ;
+            org = {
+                "key": key,
+                "name": item1.A,                    
+                "pid": pid,
+                "pos": pos
+            } ;
+        } ;
+        //console.log('org = ', org) ;
+        orgs_json.results.bindings.push(org) ;                
+    }
+}) ;
+
+//read index json files
+jsonJs_in = jsonJs_reg_files['register_index_text'] ; //index.json
+jsonJs_in_xlsx = jsonJs_reg_files['register_index_xlsx'] ; //index_xlsx.json
+
+//group by key                
+groupedByKey = jsonJs_in.results.bindings.groupBy( item => {  //index_text.json
+    return item.o_main_index.value ;    
+}) ;        
+groupedByKey_xlsx = jsonJs_in_xlsx.Tabelle1.groupBy( item => {  //index_xlsx.json
+    return item.A ;
+}) ;
+
 
 //read json register directory
 let jsonFiles = fs.readdirSync('data/json/register/') ;
