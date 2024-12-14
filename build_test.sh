ecc83fff825ce4a3abf7befed0b7f2c9441d21df
rh <richard.hoermann@inode.at>
2024-12-14 21:41:03 +0100

    update data

--- build_test.sh
+++ build_test.sh
@@ -1,60 +1,6 @@
 echo "build test"
 
-echo "clear graphdb"
-./gdb_clearRepo.sh
-
-echo "import text files to graphdb"
-echo read files from ttl directory
-inputDir="data/ttl/text/"
-
-for file in $inputDir*.ttl; do
-  #echo "$file"
-  #check if file is no template
-    if [[ "$file" = *"Bae_"*  ]]
-    then
-        echo "Found ttl: ${file}"
-        pathname=$(dirname "$file")/        
-        echo "ttl path: ${pathname}"
-        name=$(basename "$file" .ttl)
-        echo "ttl filename: ${name}"  #name of the file without extension
-        if test -f "$file"
-        then
-        echo "Starting ttl import to graphdb repo"
-        #export pathName
-        export pathName=$pathname
-        export name=$name                
-        ./gdb_importFile.sh
-        fi
-    fi
-done
-
-echo "build test ttl"
-export mime_type='text/turtle'
-export query_type='CONSTRUCT'
-export path_rq='assets/staticSrc/sparql/'
-export file_rq='test'
-export path_out='./data/ttl/annotation/anno_web/instance/'
-export file_out='testi'
-export ext_out='.ttl'
-#./gdb_queryRepo.sh
-
-echo "clear graphdb"
-#./gdb_clearRepo.sh
-
-echo "import test ttl to graphdb"
-echo "Starting ttl import to graphdb repo"
-export pathName='data/ttl/annotation/anno_web/instance/'
-export name='testi'                
-#./gdb_importFile.sh
-
-echo "build test json"
-export mime_type='application/sparql-results+json'
-export query_type='SELECT'
-export path_rq='assets/staticSrc/sparql/'
-export file_rq='listXmlElements_text'
-export path_out='./data/json/'
-export file_out='listXmlElements_text'
-export ext_out='.json'
-./gdb_queryRepo.sh
-
-echo "build test done!"
+./build_texts.sh
+./build_register.sh
+./build_ssSearch_all.sh
+./build_html.sh
