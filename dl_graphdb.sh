57a0227d3052590ac08bb668f201631593b91b22
rH1967 <richard.hoermann@inode.at>
2024-12-27 23:28:09 +0100

    build .sh

---
+++ dl_graphdb.sh
@@ -0,0 +1,8 @@
+echo "install graphdb client"
+npm i @innotrade/enapso-graphdb-client --save
+
+echo "install graphdb admin"
+npm i @innotrade/enapso-graphdb-admin --save
+
+echo "install graphdb.js"
+npm i graphdb
