4997a38af16f4c70453af997d5a2461e42af4034
rh <richard.hoermann@inode.at>
2025-01-03 23:11:55 +0100

    clear install.sh

--- install.sh
+++ install.sh
@@ -19,12 +19,3 @@ npm install convert-excel-to-json
 
 echo "Install core-js"
 npm install core-js
-
-#COMMENTED OUT FOR NOW
-#npm install aspose.cells
-
-#echo "install localtunnel"
-#npm install -g localtunnel
-
-#https://github.com/localtunnel/localtunnel
-#https://fleetdm.com/engineering/tips-for-github-actions-usability
