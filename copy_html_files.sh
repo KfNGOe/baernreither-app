696451f90e0d611caffad586b99346617ec0bf3c
rh <richard.hoermann@inode.at>
2025-01-04 17:38:53 +0100

    deploy .sh

---
+++ copy_html_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Source and destination directories
+src_dir="/home/rh/github/dev-baernreither-app/"
+dest_dir="/home/rh/github/baernreither-app/"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$dest_dir"
+
+# Find and copy .js files that start with "gdb_" but exclude those that start with "gdb_test"
+find "$src_dir" -type f -name "build_*_html.sh" ! -exec cp {} "$dest_dir" \;
+
+echo "Copy completed."
