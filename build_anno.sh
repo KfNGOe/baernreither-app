b61c4d73a9190ac65eaf7a842f8a66b616496fc6
rh <richard.hoermann@inode.at>
2025-01-04 16:16:22 +0100

    fix bug

--- build_anno.sh
+++ build_anno.sh
@@ -2,11 +2,11 @@ echo "build anno levels"
 
 for script in ./build_anno*.sh; 
 do 
-  echo "Running $script"^
+  echo "Running $script"
   #stop to read the output
-  #read -n 1    
+  read -n 1    
   #run the .sh scripts with exceptions
-  if [ -f "$script" ] && [ "$script" != "./build_anno.sh" ] && ["$script" != "./build_annoIndexSub.sh"] && [ "$script" != "./build_annoTextFull.sh" ] && [ "$script" != "./build_annoRegister.sh" ] ; then
+  if [ -f "$script" ] && [ "$script" != "./build_anno.sh" ] && [ "$script" != "./build_annoIndexSub.sh" ] && [ "$script" != "./build_annoTextFull.sh" ] && [ "$script" != "./build_annoRegister.sh" ]; then
     ./"$script"
   fi
 done
