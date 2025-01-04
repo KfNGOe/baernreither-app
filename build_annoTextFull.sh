915f65640ed6048a36dc69b0167aa1990c4e8b17
rh <richard.hoermann@inode.at>
2025-01-04 12:24:49 +0100

    fix bug

--- build_annoTextFull.sh
+++ build_annoTextFull.sh
@@ -10,9 +10,9 @@ inputDir_tmp="data/ttl/anno/anno_web/instance/tmp/"
 #make tmp dir
 mkdir -p $inputDir_tmp
 #copy files to tmp dir
-cp $inputDir*annoAbbri.ttl $inputDir*annoAppi.ttl $inputDir*annoDeli.ttl $inputDir*annoNotei.ttl $inputDir_tmp
-#$inputDir*annoIndexSubi.ttl 
+cp $inputDir/annoAbbri.ttl $inputDir/annoAppi.ttl $inputDir/annoDeli.ttl $inputDir/annoNotei.ttl $inputDir_tmp
 
+#read files from tmp directory
 for file in $inputDir_tmp*.ttl; do
   #echo "$file"
   #check if file is no template
