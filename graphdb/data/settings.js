3cf8654fe2ea246ae00a389a59491e4b301fee14
rh <richard.hoermann@inode.at>
2025-03-01 17:13:58 +0100

    change tei

--- graphdb/data/settings.js
+++ graphdb/data/settings.js
@@ -2,7 +2,66 @@
   "properties" : {
     "current.location" : ""
   },
-  "import.local" : { },
+  "import.local" : {
+    "kfngoe;;Bae_TB_1.ttl" : {
+      "name" : "Bae_TB_1.ttl",
+      "status" : "ERROR",
+      "message" : "org.eclipse.rdf4j.sail.SailException: Invalid IRI value",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/Bae_TB_1.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "bb9570f1-9fca-44bf-9a26-1d84518c6c80",
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true,
+        "contextLink" : null
+      },
+      "size" : "4676998",
+      "lastModified" : 1740835471780,
+      "imported" : 1740835472366,
+      "addedStatements" : 0,
+      "removedStatements" : 0,
+      "numReplacedGraphs" : 0
+    },
+    "kfngoe;;Bae_TB_2.ttl" : {
+      "name" : "Bae_TB_2.ttl",
+      "status" : "ERROR",
+      "message" : "org.eclipse.rdf4j.sail.SailException: Invalid IRI value",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/Bae_TB_2.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "dcc0aedf-0bd4-48cf-8923-d48bb0c0d610",
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true,
+        "contextLink" : null
+      },
+      "size" : "4802383",
+      "lastModified" : 1740835504060,
+      "imported" : 1740835504145,
+      "addedStatements" : 0,
+      "removedStatements" : 0,
+      "numReplacedGraphs" : 0
+    }
+  },
   "locations" : {
     "" : {
       "location" : "",
---
+++ test/concatenate_text.sh
@@ -0,0 +1,2 @@
+echo "concatenate text"
+node /home/rh/github/dev-baernreither-app/test/js/concatenate_text.js
