221dbc254bb7fb1af180845cc8145e4ec8c98606
rh <richard.hoermann@inode.at>
2025-03-01 16:29:26 +0100

    reset repo

--- html/js/index.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    //ul.navbar-nav li.nav-item a.nav-link.active                          
-    //$("ul.navbar-nav li.nav-item a.nav-link").removeClass("active");
-    //$( "[href='index.html']" ).addClass("active");
-}) ;
