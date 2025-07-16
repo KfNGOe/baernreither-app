02ab8eea794a7c70cf2539939a7662c4d0211930
rh <richard.hoermann@inode.at>
2025-07-16 14:05:06 +0200

    change file

--- build_tei2ttl.sh
+++ build_tei2ttl.sh
@@ -13,6 +13,13 @@ EXTENSION_XML='.xml'
 EXTENSION_TTL='.ttl'
 EXTENSION_JSON='.json'
 
+FILEPATH_TEI=$PATH_TEI$FILENAME$EXTENSION_XML
+FILEPATH_TEI_XMLID=$PATH_TEI_XMLID$FILENAME$EXTENSION_XML
+FILEPATH_JSON_XMLID=$PATH_JSON_XMLID$FILENAME$EXTENSION_JSON
+FILEPATH_JSON_XMLJS=$PATH_JSON_XMLJS$FILENAME$EXTENSION_JSON
+FILEPATH_JSON_RDF=$PATH_JSON_RDF$FILENAME$EXTENSION_JSON
+FILEPATH_TTL=$PATH_TTL$FILENAME$EXTENSION_TTL
+
 echo "create temporary paths"
 mkdir -p "$PATH_TEI_XMLID"
 mkdir -p "$PATH_JSON_XMLID"
@@ -21,24 +28,26 @@ mkdir -p "$PATH_JSON_RDF"
 mkdir -p "$PATH_TTL"
 
 echo "normalize whitespace"
-path=$PATH_TEI file=$FILENAME ext=$EXTENSION_XML node assets/staticSrc/js/normalize_ws.js
+filepath_in_tei=$FILEPATH_TEI node assets/staticSrc/js/normalize_ws.js
 
 echo "build xml ID"
-path_in_tei=$PATH_TEI path_out_json=$PATH_JSON_XMLID path_out_tei=$PATH_TEI_XMLID file=$FILENAME ext_xml=$EXTENSION_XML ext_json=$EXTENSION_JSON node assets/staticSrc/js/build_xmlId.js
+filepath_in_tei=$FILEPATH_TEI filepath_out_tei=$FILEPATH_TEI_XMLID filepath_out_json=$FILEPATH_JSON_XMLID node assets/staticSrc/js/build_xmlId.js
 
 echo "build xml JS"
-path_in_tei=$PATH_TEI_XMLID path_out_json=$PATH_JSON_XMLJS file=$FILENAME ext_xml=$EXTENSION_XML ext_json=$EXTENSION_JSON node assets/staticSrc/js/build_xmlJs.js
+filepath_in_tei=$FILEPATH_TEI_XMLID filepath_out_json=$FILEPATH_JSON_XMLJS node assets/staticSrc/js/build_xmlJs.js
 
 echo "build rdf JS"
-path_in_json=$PATH_JSON_XMLJS path_out_json=$PATH_JSON_RDF file=$FILENAME ext_json=$EXTENSION_JSON node assets/staticSrc/js/build_rdfJs.js
+filepath_in_json=$FILEPATH_JSON_XMLJS filepath_out_json=$FILEPATH_JSON_RDF node assets/staticSrc/js/build_rdfJs.js
 
 echo "build ttl"
-path_in_json=$PATH_JSON_RDF path_out_ttl=$PATH_TTL file=$FILENAME ext_json=$EXTENSION_JSON ext_ttl=$EXTENSION_TTL node assets/staticSrc/js/build_ttl.js
+filepath_in_json=$FILEPATH_JSON_RDF filepath_out_ttl=$FILEPATH_TTL node assets/staticSrc/js/build_ttl.js
 
 echo copy tei_xmlId to tei
 cp $PATH_TEI_XMLID$FILENAME$EXTENSION_XML $PATH_TEI$FILENAME$EXTENSION_XML
+cp $FILEPATH_TEI_XMLID $FILEPATH_TEI
 
 echo "remove temporary paths"
+rm -r $PATH_TEI_XMLID
 rm -r $PATH_JSON_XMLID
 rm -r $PATH_JSON_XMLJS
 rm -r $PATH_JSON_RDF
